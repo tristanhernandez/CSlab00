c52dcc9687660b2490922a72978805b4b13fa1ca
Jennifer Lo <jenniferlo@Jennifers-MacBook-Air-2.local>
2018-10-05 17:37:05 -0700

    Hello World file

---
+++ hello.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+//include a library for input output
+using namespace std;
+//allows referring to specific modules in the standard namespace
+
+//int fobar(){
+//	return 5;
+//}
+
+int main() {
+	//Execution always starts with main()
+	//int x; //Declare the variable x
+	//x = 3; //Initialize x
+	
+	//int y = 10;
+	//int z; 
+	
+	//string name;
+	//name = "Jennifer";
+
+	//cout<< "Hello CS16!"<<name6<<endl;
+	//foobar();
+	//return 0;
+	//Class Lecture Notes
+
+	cout << "Hello, world!" << endl;
+	cout << "I am ready for CS16!" <<endl;
+	return 0;
+}
+
+
+
+}
+
+
